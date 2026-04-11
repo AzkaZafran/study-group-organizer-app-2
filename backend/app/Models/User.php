dccda404312a44c7a8ecc088f48fe5a31b0911cf
AzkaZafran <azkazafran777@gmail.com>
2026-04-11 21:58:40 +0700

    membuat user model

---
+++ backend/app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = "users";
+    protected $primaruKey = "id";
+    protected $keyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+}
