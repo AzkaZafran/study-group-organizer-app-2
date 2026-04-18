d188715600b7f7fa2f5f7be6c76a05b997f6b432
AzkaZafran <azkazafran777@gmail.com>
2026-04-18 14:42:37 +0700

    implementasi fungsi yang diperlukan untuk user model implement Authenticatable

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
     protected $table = "users";
     protected $primaryKey = "id";
@@ -18,4 +19,32 @@ class User extends Model
         'email',
         'is_verified'
     ];
+
+    public function getAuthIdentifier() {
+        return $this->id;
+    }
+
+    public function getAuthPassword() {
+        return $this->password;
+    }
+
+    public function getAuthIdentifierName() {
+        return 'id';
+    }
+
+    public function getAuthPasswordName() {
+        return 'password';
+    }
+
+    public function getRememberToken() {
+        return $this->token;
+    }
+
+    public function getRememberTokenName() {
+        return 'token';
+    }
+
+    public function setRememberToken($value) {
+        $this->token = $value;
+    }
 }
