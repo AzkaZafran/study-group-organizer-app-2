62a0221219f73c525adb54caa449ef0819adc267
AzkaZafran <azkazafran777@gmail.com>
2026-08-27 12:26:32 +0700

    Mengganti link css pada blade app layout dengan wrapper vite + menghapus tailwind pada app.css

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -5,9 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Study Group Organizer</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <link href="{{ assert('css/app.css') }}" rel="stylesheets">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @stack('styles')
-        @vite('resources/js/app.js')
     </head>
     <body class="d-flex flex-column min-vh-100" style="background-color: #F8FAFC;">
         @yield('content')
